32fc25c0482e704645f0c9674bcabb49a8f95e69
Roman Slysh <monar.ua@gmail.com>
2016-05-09 13:36:56 +0300

    Initial Commit

---
+++ Sadhana/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Sadhana
+//
+//  Created by Roman Slysh on 5/9/16.
+//  Copyright © 2016 Roman Slysh. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
